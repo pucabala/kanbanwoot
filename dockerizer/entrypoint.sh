f3b4b41b827eeee6e8e2718cf9783e8445a8c0cf
pucabala <publiomelo@gmail.com>
2025-05-23 16:43:34 +0000

    Update entrypoint.sh to generate .env file instead of .env.js and adjust variable format

--- dockerizer/entrypoint.sh
+++ dockerizer/entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # Define o caminho do arquivo .env na raiz do projeto servido
-ENV_FILE="/usr/share/nginx/html/.env.js"
+ENV_FILE="/usr/share/nginx/html/.env"
 
 echo "Gerando arquivo de variáveis de ambiente: $ENV_FILE"
 
 # Cria o arquivo .env com base nas variáveis do ambiente disponíveis no container
 cat <<EOF > "$ENV_FILE"
-window._env_ = {
+
   REACT_APP_CHATWOOT_URL: "${REACT_APP_CHATWOOT_URL}",
   REACT_APP_CHATWOOT_TOKEN: "${REACT_APP_CHATWOOT_TOKEN}",
   REACT_APP_CHATWOOT_ACCOUNT_ID: "${REACT_APP_CHATWOOT_ACCOUNT_ID}",
   REACT_APP_DEBUG: "${REACT_APP_DEBUG}"
-};
+
 EOF
 
 echo "Arquivo .env gerado com sucesso com os seguintes valores:"
