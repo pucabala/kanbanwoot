f701ca80a452bf62eaa2c7b579ffceb97ee1ecc7
pucabala <publiomelo@gmail.com>
2025-05-23 14:59:29 +0000

    Add TesteContacts component for displaying contacts and update App routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import KanbanBoard from './components/KanbanBoard';
+import TesteContacts from './components/TesteContacts';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { debugLog } from './debug';
 
 function App() {
   debugLog('App.jsx: App renderizado');
   return (
-    <div>
-      <header>
-        <h1 className="text-center text-2xl font-bold mt-4">Kanban - Chatwoot</h1>
-      </header>
-      <main>
-        <KanbanBoard />
-      </main>
-    </div>
+    <Router>
+      <div>
+        <header>
+          <h1 className="text-center text-2xl font-bold mt-4">Kanban - Chatwoot</h1>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<KanbanBoard />} />
+            <Route path="/teste" element={<TesteContacts />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
