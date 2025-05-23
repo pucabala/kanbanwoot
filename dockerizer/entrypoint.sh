eee1515dcaccba09dc9d307fa9c9a8bd000cb6e2
pucabala <publiomelo@gmail.com>
2025-05-23 12:38:07 +0000

    Refactor entrypoint script to generate .env file instead of env.js for environment variables

--- dockerizer/entrypoint.sh
+++ dockerizer/entrypoint.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
-# Define o caminho do arquivo env.js no diretório onde o React é servido
-ENV_FILE="/usr/share/nginx/html/env.js"
+# Define o caminho do arquivo .env na raiz do projeto servido
+ENV_FILE="/usr/share/nginx/html/.env"
 
 echo "Gerando arquivo de variáveis de ambiente: $ENV_FILE"
 
-# Cria o arquivo JS com base nas variáveis do ambiente disponíveis no container
+# Cria o arquivo .env com base nas variáveis do ambiente disponíveis no container
 cat <<EOF > "$ENV_FILE"
-window._env_ = {
-  REACT_APP_API_URL: "${REACT_APP_API_URL}",
-  REACT_APP_CHATWOOT_TOKEN: "${REACT_APP_CHATWOOT_TOKEN}",
-  REACT_APP_ACCOUNT_ID: "${REACT_APP_ACCOUNT_ID}"
-};
+CHATWOOT_URL=${CHATWOOT_URL}
+CHATWOOT_TOKEN=${CHATWOOT_TOKEN}
+CHATWOOT_ACCOUNT_ID=${CHATWOOT_ACCOUNT_ID}
 EOF
 
-echo "Arquivo env.js gerado com sucesso com os seguintes valores:"
+echo "Arquivo .env gerado com sucesso com os seguintes valores:"
 cat "$ENV_FILE"
 
 # Inicia o Nginx
