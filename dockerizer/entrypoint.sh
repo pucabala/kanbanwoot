8406da2caba0744ed645d2fc8b09127bfec9ef8f
pucabala <publiomelo@gmail.com>
2025-05-23 18:42:39 +0000

    Fix environment file path and restore env.js for frontend configuration

--- dockerizer/entrypoint.sh
+++ dockerizer/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Define o caminho do arquivo .env.js na raiz do projeto servido
-ENV_FILE="/usr/share/nginx/html/.env.js_"
+ENV_FILE="/usr/share/nginx/html/env.js_"
 
 echo "Gerando arquivo de variáveis de ambiente: $ENV_FILE"
 
