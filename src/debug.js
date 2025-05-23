9934305f5aa1e5ff265a5a0c300473c1d59547d9
pucabala <publiomelo@gmail.com>
2025-05-23 04:37:42 +0000

    Enhance debug logging to write output to stdout in Node.js environment

--- src/debug.js
+++ src/debug.js
@@ -4,5 +4,10 @@ export function debugLog(...args) {
   if (isDebug) {
     // eslint-disable-next-line no-console
     console.log('[DEBUG]', ...args);
+
+    // Também escreve no /dev/stdout se estiver em ambiente Node.js
+    if (typeof process !== 'undefined' && process.stdout && process.stdout.write) {
+      process.stdout.write('[DEBUG] ' + args.map(String).join(' ') + '\n');
+    }
   }
 }
