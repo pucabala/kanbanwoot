b23d154d60387d84b4bcba17188757232c11169f
pucabala <publiomelo@gmail.com>
2025-05-23 16:34:01 +0000

    Update environment variable references in entrypoint.sh to use REACT_APP prefix for consistency

--- dockerizer/entrypoint.sh
+++ dockerizer/entrypoint.sh
@@ -8,9 +8,9 @@ echo "Gerando arquivo de variáveis de ambiente: $ENV_FILE"
 # Cria o arquivo .env com base nas variáveis do ambiente disponíveis no container
 cat <<EOF > "$ENV_FILE"
 window._env_ = {
-  REACT_APP_CHATWOOT_URL: "${CHATWOOT_URL}",
-  REACT_APP_CHATWOOT_TOKEN: "${CHATWOOT_TOKEN}",
-  REACT_APP_CHATWOOT_ACCOUNT_ID: "${CHATWOOT_ACCOUNT_ID}",
+  REACT_APP_CHATWOOT_URL: "${REACT_APP_CHATWOOT_URL}",
+  REACT_APP_CHATWOOT_TOKEN: "${REACT_APP_CHATWOOT_TOKEN}",
+  REACT_APP_CHATWOOT_ACCOUNT_ID: "${REACT_APP_CHATWOOT_ACCOUNT_ID}",
   REACT_APP_DEBUG: "${REACT_APP_DEBUG}"
 };
 EOF
