71eb0efd147516a4f67fc9a6b12c74e4833574e4
pucabala <publiomelo@gmail.com>
2025-05-23 16:31:00 +0000

    Refactor environment variable handling to use window._env_ and update .env.js script inclusion in index.html

--- dockerizer/entrypoint.sh
+++ dockerizer/entrypoint.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 # Define o caminho do arquivo .env na raiz do projeto servido
-ENV_FILE="/usr/share/nginx/html/.env"
+ENV_FILE="/usr/share/nginx/html/.env.js"
 
 echo "Gerando arquivo de variáveis de ambiente: $ENV_FILE"
 
 # Cria o arquivo .env com base nas variáveis do ambiente disponíveis no container
 cat <<EOF > "$ENV_FILE"
-REACT_APP_CHATWOOT_URL=${CHATWOOT_URL}
-REACT_APP_CHATWOOT_TOKEN=${CHATWOOT_TOKEN}
-REACT_APP_CHATWOOT_ACCOUNT_ID=${CHATWOOT_ACCOUNT_ID}
-REACT_APP_DEBUG=${REACT_APP_DEBUG}
+window._env_ = {
+  REACT_APP_CHATWOOT_URL: "${CHATWOOT_URL}",
+  REACT_APP_CHATWOOT_TOKEN: "${CHATWOOT_TOKEN}",
+  REACT_APP_CHATWOOT_ACCOUNT_ID: "${CHATWOOT_ACCOUNT_ID}",
+  REACT_APP_DEBUG: "${REACT_APP_DEBUG}"
+};
 EOF
 
 echo "Arquivo .env gerado com sucesso com os seguintes valores:"
--- public/.env.js
+++ public/.env.js
@@ -5,3 +5,8 @@ window._env_ = {
   REACT_APP_CHATWOOT_URL: 'https://whats.agente.live',
   REACT_APP_DEBUG: 'true',
 };
+// O arquivo .env.js é injetado no build do React, então não é necessário usar dotenv ou outro pacote para carregar variáveis de ambiente.
+// As variáveis de ambiente são acessadas através de window._env_ no código do React.
+// Isso permite que você tenha um arquivo de configuração separado para o frontend, sem expor as variáveis de ambiente no código-fonte.
+// Você pode usar o arquivo .env.js para definir variáveis de ambiente específicas para o frontend, como a URL da API do Chatwoot, o ID da conta e o token de acesso.
+// Isso é útil para evitar expor informações sensíveis no código-fonte e para facilitar a configuração do ambiente de desenvolvimento e produção.
