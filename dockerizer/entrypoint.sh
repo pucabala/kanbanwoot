53b89a03411489348eed3c1e0b2507c69738984e
pucabala <publiomelo@gmail.com>
2025-05-24 01:33:38 +0000

    Fix environment file path and ensure correct variable expansion in entrypoint script

--- dockerizer/entrypoint.sh
+++ dockerizer/entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # Define o caminho do arquivo .env.js na raiz do projeto servido
-ENV_FILE="/usr/share/nginx/html/env.js_"
+ENV_FILE="/usr/share/nginx/html/env.js"
 
 echo "Gerando arquivo de variáveis de ambiente: $ENV_FILE"
 
 # Cria o arquivo .env.js com base nas variáveis do ambiente disponíveis no container
 cat <<EOF > "$ENV_FILE"
 window._env_ = {
-  REACT_APP_CHATWOOT_URL: "${REACT_APP_CHATWOOT_URL}",
-  REACT_APP_CHATWOOT_TOKEN: "${REACT_APP_CHATWOOT_TOKEN}",
-  REACT_APP_CHATWOOT_ACCOUNT_ID: "${REACT_APP_CHATWOOT_ACCOUNT_ID}",
-  REACT_APP_DEBUG: "${REACT_APP_DEBUG}"
+  REACT_APP_CHATWOOT_URL: "$REACT_APP_CHATWOOT_URL",
+  REACT_APP_CHATWOOT_TOKEN: "$REACT_APP_CHATWOOT_TOKEN",
+  REACT_APP_CHATWOOT_ACCOUNT_ID: "$REACT_APP_CHATWOOT_ACCOUNT_ID",
+  REACT_APP_DEBUG: "$REACT_APP_DEBUG"
 };
 EOF
 
