fba0b44b8d38a393ad6cd19b3025cebdf481f400
pucabala <publiomelo@gmail.com>
2025-05-24 01:36:50 +0000

    Reset environment variables in env.js for development purposes

--- public/env.js
+++ public/env.js
@@ -1,9 +1,8 @@
 //.env.js
 // Este arquivo substitui o uso do .env para variáveis de ambiente no frontend
 window._env_ = {
-  REACT_APP_CHATWOOT_TOKEN: 'xvKatAgC67jwYwj2gbqW5LJm',
-  REACT_APP_CHATWOOT_ACCOUNT_ID: '2',
-  REACT_APP_CHATWOOT_URL: 'https://whats.agente.live',
+  REACT_APP_CHATWOOT_TOKEN: 'x',
+  REACT_APP_CHATWOOT_ACCOUNT_ID: '',
+  REACT_APP_CHATWOOT_URL: '',
   REACT_APP_DEBUG: 'true',
-  REACT_APP_DEBUG1: 'true',
 };
